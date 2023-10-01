da9d2020bdb3f3e3ebe4ff1d190f77a898eb1417
Adharsh <71386788+adharsh2608@users.noreply.github.com>
2023-10-01 19:31:58 -0400

    Time: 14 ms (29.11%), Space: 44.6 MB (23.45%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -0,0 +1,46 @@
+class Solution {
+    public String convert(String a, int rows) {
+       
+        boolean flag=true;
+        int i=0;
+        String[] arr=new String[rows];
+        for(int k=0;k<rows;k++)
+        {
+            arr[k]="";
+        }
+        int j=0;
+        if(rows==1)
+            return a;
+        while(i<a.length())
+        {
+            if(flag)
+            {
+                arr[j]=arr[j]+a.charAt(i);
+                j++; i++;
+                if(j==rows-1)
+                {
+                    flag=false;
+                }
+            }
+            if(!flag&&i<a.length())
+            {
+                arr[j]=arr[j]+a.charAt(i);
+                
+                j--;i++;
+                if(j==0)
+                {
+                    flag=true;
+                }
+            }
+            
+        }
+        String ans="";
+        for(String k:arr)
+        {
+            ans=ans+k;
+        }
+    return ans;
+    }
+        
+    
+}
