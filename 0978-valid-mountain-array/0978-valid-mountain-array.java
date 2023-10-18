be9ebccd2484c2929d7f3f729a9a06256bde5d3c
Adharsh <71386788+adharsh2608@users.noreply.github.com>
2023-10-18 04:09:16 -0700

    Time: 4 ms (6.36%), Space: 44.4 MB (78.58%) - LeetHub

---
+++ 0978-valid-mountain-array/0978-valid-mountain-array.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean validMountainArray(int[] arr) {
+    int a=arr.length;
+		boolean flag=true;
+		if (a <= 2 || a > Math.pow(10, 4)) {
+			flag = false;
+		} else if (arr[0] >= arr[1]) {
+			flag = false;
+		} else {
+			int j = 0;
+			for (int i = 0; i < arr.length - 1; i++) {
+				if (arr[i] >= arr[i + 1]) {
+					j = i;
+					break;
+				}
+			}
+
+			for (int i = j; i < arr.length - 1; i++) {
+				if (arr[i] <= arr[i + 1]) {
+					flag = false;
+					break;
+				}
+			}
+
+			for (int i : arr) {
+				if (i < 0 || i > Math.pow(10, 4)) {
+					flag = false;
+					break;
+				}
+			}
+		}
+
+		return flag;
+    }
+}
