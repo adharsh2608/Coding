31ca4e127783bf0c6506125f35feb5b68f5d6bf9
Adharsh <71386788+adharsh2608@users.noreply.github.com>
2025-01-27 23:39:30 +0530

    Time: 0 ms (100%), Space: 43.1 MB (68.78%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,21 +1,26 @@
 public class Solution {
+    private void backtrack(int openN, int closedN, int n, List<String> res, StringBuilder stack) {
+        if (openN == closedN && openN == n) {
+            res.add(stack.toString());
+            return;
+        }
+
+        if (openN < n) {
+            stack.append('(');
+            backtrack(openN + 1, closedN, n, res, stack);
+            stack.deleteCharAt(stack.length() - 1);
+        }
+        if (closedN < openN) {
+            stack.append(')');
+            backtrack(openN, closedN + 1, n, res, stack);
+            stack.deleteCharAt(stack.length() - 1);
+        }
+    }
+
     public List<String> generateParenthesis(int n) {
-        List<List<String>> res = new ArrayList<>();
-        for (int i = 0; i <= n; i++) {
-            res.add(new ArrayList<>());
-        }
-        res.get(0).add("");
-
-        for (int k = 0; k <= n; k++) {
-            for (int i = 0; i < k; i++) {
-                for (String left : res.get(i)) {
-                    for (String right : res.get(k - i - 1)) {
-                        res.get(k).add("(" + left + ")" + right);
-                    }
-                }
-            }
-        }
-
-        return res.get(n);
+        List<String> res = new ArrayList<>();
+        StringBuilder stack = new StringBuilder();
+        backtrack(0, 0, n, res, stack);
+        return res;
     }
 }
