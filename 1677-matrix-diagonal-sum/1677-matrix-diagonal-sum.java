3bd45ce63ea43ad8c24593f1d570e6c1171deb61
Adharsh <71386788+adharsh2608@users.noreply.github.com>
2025-06-22 22:38:00 -0400

    Time: 0 ms (100%), Space: 44.2 MB (77.59%) - LeetHub

--- 1677-matrix-diagonal-sum/1677-matrix-diagonal-sum.java
+++ 1677-matrix-diagonal-sum/1677-matrix-diagonal-sum.java
@@ -8,7 +8,6 @@ class Solution {
                 sum+=mat[k][k];
                 bsum+=mat[k][len-k-1];
             }           
-            System.out.println(bsum);
         if(len%2!=0)
         {bsum=bsum-mat[(len/2)][(len/2)];}
         return sum+bsum;
